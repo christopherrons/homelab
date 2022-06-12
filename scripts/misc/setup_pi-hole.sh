38f908b8e003a68538a3d00b53ae39181e4c33ef
christopherrons <christopherheron09@gmail.com>
2022-06-12 17:11:28 +0200

    fixes

--- scripts/misc/setup_pi-hole.sh
+++ scripts/misc/setup_pi-hole.sh
@@ -13,7 +13,7 @@ fi
 ssh -tt "$ROOT_USER"@"$LXC_IP" 'sudo curl -sSL https://install.pi-hole.net | bash'
 ssh -tt "$ROOT_USER"@"$LXC_IP" 'pihole -a -p'
 
-
+echo "Add dns names and ip's"
 ssh -tt "$ROOT_USER"@"$LXC_IP" "echo $HOMELAB_SERVER_IP $HOMELAB_SERVER_NAME.$LOCAL_DOMAIN >> /etc/pihole/custom.list"
 ssh -tt "$ROOT_USER"@"$LXC_IP" "echo $DOCKER_LXC_IP $DOCKER_LXC_NAME.$LOCAL_DOMAIN >> /etc/pihole/custom.list"
 ssh -tt "$ROOT_USER"@"$LXC_IP" "echo $JENKINS_MASTER_LXC_IP $JENKINS_MASTER_LXC_NAME.$LOCAL_DOMAIN >> /etc/pihole/custom.list"
